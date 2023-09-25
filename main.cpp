d8be72f42d563c5e2d5c7af40c029fb791bdfc92
Kelvin Appiah <kelpyshades060904@gmail.com>
2023-09-25 01:34:54 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int a = 20;
+    int b = 5;
+    int c = 10;
+    float x = 2.5;
+    float y = 1.0;
+    float z = 20.0;
+    float d;
+
+    d = 2%a+x/3*z;
+    cout <<d<< endl;
+    return 0;
+}
